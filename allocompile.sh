cc57b4980d4e282d7024115d44f6cc39fe455759
Pablo Colapinto <wolftype@gmail.com>
2014-03-13 18:07:32 -0700

    knot darwing

--- allocompile.sh
+++ allocompile.sh
@@ -10,10 +10,12 @@ echo Compiling $1 for ALLOSPHERE
 DIRECTORY=`echo $1 |cut -d'/' -f1`
 FILENAME=`echo $1 |cut -d'/' -f2 |cut -d'.' -f1 | sed -e "s|/|_|g"`
 TARGET=${FILENAME}
-echo target name $TARGET
+echo Directory is $DIRECTORY
+echo Filename is $FILENAME
+echo Target is $TARGET
 
 mkdir build
 cd build
 rm bin/${TARGET}
-cmake .. -DSRC=../../$1 -DALLOSPHERE=1
+cmake .. -DDIR=${DIRECTORY} -DSRC=../../$1 -DALLOSPHERE=1
 make
