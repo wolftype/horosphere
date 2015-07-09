a31c36a877b579f7b6fe88e6baab732c171d1557
wolftype <wolftype@gmail.com>
2015-07-09 13:39:04 -0700

    knot.sh script adds interface

--- knot.sh
+++ knot.sh
@@ -1,5 +1,7 @@
 #1/bin/bash
 
 ./current/xKnotAudio &
-./current/xKnotGLV
+./current/xKnotGLV &
+cd ~/code/interface.js/server/ &&
+node interface.simpleserver.js --serverPort 10080 --oscOutPort 8082 --interfaceDirectory ~/Documents/interfaces
 
